40d1bedcf6a8065b7478e3a2069e63ccc14183d2
phil-sorenson <phillip.sorenson31@gmail.com>
2022-12-22 01:06:23 -0600

    Created new views.py function to Get song by ID (song_detail) +Seperated POST in to its own function (post_song) and GET (get_songs_list) + Added url paths for each views.py function

--- music_library_project/urls.py
+++ music_library_project/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.songs_list),
+    path('', views.get_songs_list),
+    path('', views.post_song),
+        # Looking to return a specific song w/ its PK/ID # so allowing 'strings' to be the PK in the URL for GET requests via can cause problems
+            # SOLUTION: 👇 int: in front of pk 
+    path('<int:pk>', views.song_detail),
 ]
--- music_library_project/views.py
+++ music_library_project/views.py
@@ -6,8 +6,8 @@ from .models import Song
 from .serializers import SongSerializer
 
 # Since POST will be using the same 'endpoint(URL)' as GET, we are able to put them in the same function
-@api_view(['GET', 'POST'])
-def songs_list(request):
+@api_view(['GET'])
+def get_songs_list(request):
     if request.method == 'GET':
             # Query to GET all of the songs from database
         songs = Song.objects.all()
@@ -17,18 +17,38 @@ def songs_list(request):
             # all information/objects queried comes back as "data"👇(hence why we want to return 'serializer.data' below)
         return Response(serializer.data, status = status.HTTP_200_OK)
 
-    elif request.method == 'POST':
+@api_view(['POST'])        
+def post_song(request):
+    if request.method == 'POST':
             # Query to CREATE/POST a new song to database/list
-            # CODE THOUGHT PROCESS: What do we have to do/implement to ACCEPT the POST/request for a new Song object
+            #! CODE THOUGHT PROCESS: What do we have to do/implement to ACCEPT the POST/request for a new Song object
                 # 'request.data' == The POST request sent in from Postman or some other API site
         serializer = SongSerializer(data = request.data)
 
             # Before accepting incoming data from POST request, We MUST VALIDATE(📝👇)--make sure it is not 'dirty' or containing incorrect information (e.g. 'name' instead of 'title')
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status = status.HTTP_201_CREATED)
+            # Built in Django/serializer functionality - 'raise_exception=True' (📝👆) is the shortened version of the code below (No 'IF Statement' needed)
+                #* if serializer.is_valid() == True:
+                #*     serializer.save()
+                #*     return Response(serializer.data, status = status.HTTP_201_CREATED)
+                #* else:
+                #*     return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def song_detail(request, pk):
+        #! CODE THOUGHT PROCESS: How can we connect this function to a specific endpoint in order to GET a specific Song object/'pk' (Primary Key)
+            #! Create a specific url path via urls.py w/ <pk> involved
+    try:   
+        song = Song.objects.get(pk=pk)
+            # get(pk=pk): Us telling Django look for the 'pk' from the songs table and return the one that is = to the pk parameter value
+                # In case the inputted 'pk' is not found in the database table, we have to wrap our code in a Try/Except
+        serializer = SongSerializer(song)
+        return Response (serializer.data)
+    except Song.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)   
+    
     
 
 
