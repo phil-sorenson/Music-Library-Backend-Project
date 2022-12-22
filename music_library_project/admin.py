1b74c618de02bb56acd6f78020befa60d958f510
phil-sorenson <phillip.sorenson31@gmail.com>
2022-12-21 21:49:46 -0600

    Created superuser in order to use the admin site + registered the admin site to our admin.py file in our music_library_prject app

--- music_library_project/admin.py
+++ music_library_project/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Song
 
-# Register your models here.
+#! In order to use the Admin site (That we got through creating the superuser) we must import our models/Song model👆 & register our Song model in 'admin.py' 👇
+admin.site.register(Song)
